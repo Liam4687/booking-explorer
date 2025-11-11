6ef9ab4f30cae23204239e2935deb2f2a3cf53c9
Liam4687 <TabathaSalgadotppyw@aservices.us>
2025-11-11 14:47:04 +0500

    feat: add booking-explorer-scraper/src/extractors/utils.py

---
+++ booking-explorer-scraper/src/extractors/utils.py
@@ -0,0 +1,10 @@
+thon
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
